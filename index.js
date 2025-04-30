c36a0cc9b8f5e3bbfbb587c823d71004076adb9d
Craig Clayton <craig__clayton@outlook.com>
2025-04-30 18:43:46 +0100

    make endpoints work with mongo

--- index.js
+++ index.js
@@ -37,14 +37,10 @@ app.get('/api/notes', (req, res) => {
   });
 });
 
-app.get('/api/notes/:id', (req, res) => {
-  const { id } = req.params;
-  const note = notes.find((n) => n.id === id);
-  if (note) {
-    res.json(note);
-  } else {
-    res.status(404).end();
-  }
+app.get('/api/notes/:id', (request, response) => {
+  Note.findById(request.params.id).then((note) => {
+    response.json(note);
+  });
 });
 
 app.delete('/api/notes/:id', (req, res) => {
@@ -78,20 +74,21 @@ const generateId = () => {
   return String(maxId + 1);
 };
 
-app.post('/api/notes', (req, res) => {
-  const body = req.body;
+app.post('/api/notes', (request, response) => {
+  const body = request.body;
+
   if (!body.content) {
-    return res.status(400).json({ error: 'Content missing' });
+    return response.status(400).json({ error: 'content missing' });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  };
+  });
 
-  notes = notes.concat(note);
-  res.json(note);
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
 });
 
 const PORT = process.env.PORT;
